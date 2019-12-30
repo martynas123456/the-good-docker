38ccc0a0a2ea046f2d167386351fcecb2cedf56e
martynas_mockus <57089911+martynas123456@users.noreply.github.com>
2019-12-30 18:53:49 +0200

    Initial commit

---
+++ wp-content/themes/bootstrapstarter/header.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="blog-nav navbar">
+			<a class="blog-nav-item" href="<?php echo home_url(); ?>/"><img id="logo" src="/localocean/wp-content/uploads/2019/10/logo-1.png"></a>
+            <a class="blog-nav-item" href="<?php echo home_url(); ?>/apie-mus/" >Apie mus</a>
+            <a class="blog-nav-item" href="<?php echo home_url(); ?>/kur-isigyti/">Kur įsigyti</a>
+            <a class="blog-nav-item" href="<?php echo home_url(); ?>/technologija/">Technologija</a>
+            <a class="blog-nav-item" href="<?php echo home_url(); ?>/sviezumas/">Šviežumas</a>
+            <a class="blog-nav-item" href="<?php echo home_url(); ?>/receptai/">Receptai</a>
+			<a class="blog-nav-item" href="<?php echo home_url(); ?>/kiti-produktai/">Kiti produktai</a>
+			<a class="blog-nav-item" href="<?php echo home_url(); ?>/kontaktai/">Kontaktai</a>
+			<a class="blog-nav-item" href="<?php echo home_url(); ?>/kur-isigyti/"><img id="pirkti" src="/localocean/wp-content/uploads/2019/10/pirkti.png"></a>
+			<div id="menu-soc-icons" style="display:none;">
+				<div style="text-align:center;">
+					<a href="https://www.instagram.com/local_ocean.lt/"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/10/index-ig.png"></a>
+					<a href="https://www.facebook.com/LocalOcean.LT/"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/10/index-fb.png"></a>
+					<a href="#"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/10/index-tw.png"></a>
+				</div>
+			</div>
+			
+        </nav>
+		
+		<div class="pos-f-t">
+			<div class="mobile-menu-logo-home">
+				<img id="mobile-menu-logo-home" src="/localocean/wp-content/uploads/2019/11/logo-mobile-home.png">
+			</div>
+			<div class="mobile-menu-logo">
+				<img id="mobile-menu-logo" src="/localocean/wp-content/uploads/2019/11/whale-01.png">
+			</div>
+			<div class="mobile-menu-rightsidealign">
+				<div class="mobile-menu-pan-home">
+					<a class="mobile-menu-links" href="<?php echo home_url(); ?>/kur-isigyti/">
+						<img id="mobile-menu-pan-home" src="/localocean/wp-content/uploads/2019/11/pirkti-raudonas.png">
+					</a>
+				</div>
+				<div class="mobile-menu-pan">
+					<a class="mobile-menu-links" href="<?php echo home_url(); ?>/kur-isigyti/">
+						<img id="mobile-menu-pan" src="/localocean/wp-content/uploads/2019/11/pirkti-raudonas.png">
+					</a>
+				</div>
+				<!-- Mygtukas mobile meniu -->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse" id="navbarToggleExternalContent">
+					<div class="mobile-menu-link-side">
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/apie-mus/" >Apie mus</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/kur-isigyti/">Kur įsigyti</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/technologija/">Technologija</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/sviezumas/">Šviežumas</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/receptai/">Receptai</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/kiti-produktai/">Kiti produktai</a>
+						<a class="mobile-menu-links" href="<?php echo home_url(); ?>/kontaktai/">Kontaktai</a>
+						<div class="mobile-menu-link-side-soc-icons">
+							<a href="https://www.instagram.com/local_ocean.lt/"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/11/index-ig-white.png"></a>
+							<a href="https://www.facebook.com/LocalOcean.LT/"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/11/index-fb-white.png"></a>
+							<a href="#"><img class="krevetes-svieziausios-social-icon" src="/localocean/wp-content/uploads/2019/11/index-tw-white.png"></a>
+						</div>
+					
+					</div>
+					<div class="mobile-menu-close-side">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+						  <span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+			  </div>
+		  </div>
+		</div>
+		</nav>
+    </div>
+</div>
+
+<div class="container-main">
+
+    
+	
